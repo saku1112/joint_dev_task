954dc48b2c57bc9ebb0b581f635f574762792a4a
saku <shikou.yoshi@gmail.com>
2021-02-22 21:20:19 +0900

    Q10~Q13の解答

--- task.rb
+++ task.rb
@@ -110,13 +110,19 @@ def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
-
+  puts "ユーザーの趣味一覧"
+  sports.flatten!
+  sports.uniq!
+  sports.each.with_index(1) do |sport,i|
+    puts "No#{i} #{sport}"
+  end
 end
 
 def q12
   data = { user: { name: "satou", age: 33 } }
 
   # 以下に回答を記載
+  p data[:user][:name]
 
 end
 
@@ -125,7 +131,8 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-
+  user_data.merge!(update_data)
+  p user_data
 end
 
 def q14
